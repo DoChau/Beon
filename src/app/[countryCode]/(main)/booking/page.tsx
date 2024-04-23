864f2378a67986b36380269fa6b7e24f775217fa
DoChau <dntamchau@gmail.com>
2024-04-23 23:19:05 +0700

    fx

--- src/app/[countryCode]/(main)/booking/page.tsx
+++ src/app/[countryCode]/(main)/booking/page.tsx
@@ -12,7 +12,7 @@ export default async function News() {
     return (
     <>
       <div className="w-full bg-theme-10 grid gap-4 xsmall:gap-6 font-futura text-theme-4">
-        <div className="Banner w-full h-fit mx-auto z-0">
+        <div className="Banner w-full h-fit xl:h-[300px] overflow-hidden mx-auto">
           <Image src="https://webnailsbucket.s3.amazonaws.com/pasch/news-banner.png" 
           alt="banner" 
           width={1440} 
@@ -25,7 +25,7 @@ export default async function News() {
         <Script type="text/javascript" src="https://plugin.mysalononline.com/Scripts/external/bookingplugin.js"></Script>
 
         <div className="w-full max-w-6xl min-h-screen mx-auto grid place-content-start justify-center overflow-hidden md:gap-6">
-            <h1 className="Services-title font-normal text-xl md:text-3xl uppercase drop-shadow-xl text-center">Booking</h1>
+            <h1 className="Services-title font-normal text-xl md:text-3xl uppercase mx-auto drop-shadow-xl">Booking</h1>
             <iframe 
               className="mb-4 bg-[url('https://webnailsbucket.s3.amazonaws.com/beon/bouncing-circles.svg')] bg-center bg-[length:50px_50px] bg-no-repeat"
               title="booking"
