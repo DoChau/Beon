83a1c775cfdcb09a8ad40d13871cbe171c8c9c77
DoChau <dntamchau@gmail.com>
2024-05-03 09:51:38 +0700

    sync weather

--- src/lib/weather-plugin/getHourlyData.ts
+++ src/lib/weather-plugin/getHourlyData.ts
@@ -6,7 +6,13 @@ export const getHourlyData = async ({
     lon: string
   }) => {
     const data = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}`
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}`,
+      {
+        method: "GET",
+        next: {
+          revalidate: 60000,
+        },
+      }
     )
     if (!data.ok) {
       throw new Error("Failed to fetch data")
